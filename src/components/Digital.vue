d85269422f2f9c13d336994d86cec42c71e9a3ec
jensengbg-linda-jansson <linda.t.jansson@gmail.com>
2021-08-18 15:55:43 +0200

    Added components
    Mob programming with Bachar. Tested and implemented components for setting a timer and showing countdown.

---
+++ src/components/Digital.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>{{ timeLeft }}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Digital',
+  props: {
+    timeLeft: String
+  }
+}
+</script>
+
+<style>
+
+</style>
