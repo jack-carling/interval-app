0e0b80bf91a4276ad5b400e1d89a79422f232203
jensengbg-linda-jansson <linda.t.jansson@gmail.com>
2021-08-20 11:28:21 +0200

    Added more components
    And made some changes to buttons
    
    Co-Authored-By: BacHaddad <70510728+BacHaddad@users.noreply.github.com>
    Co-Authored-By: Jack Carling <jack@carling.se>

---
+++ src/components/TimesUp.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="times-up">
+    <img src="../assets/bell.svg" alt="" />
+    <h1>Time's up!</h1>
+    <button @click="$emit('reset-timer')" class="new-time">
+      set new timer
+    </button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TimesUp',
+};
+</script>
+
+<style scoped></style>
