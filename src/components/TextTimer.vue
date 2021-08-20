aa538b5703de7fc1a4e4ba0b640a72242fe6c43a
BacHaddad <br.yhaddad@gmail.com>
2021-08-20 23:43:31 +0200

    final touchs

--- src/components/TextTimer.vue
+++ src/components/TextTimer.vue
@@ -1,63 +1,177 @@
 <template>
   <section class="digital">
-    <h1 v-if="this.minutes" > {{minutesText[minutes]}} <span ref="toSingleMinute"></span> och {{secondsText[seconds]}} <span ref="toSingleSecond"></span> kvar</h1>
-    
-      </section>
+    <h1>
+      {{ minutesText[minutes] }} <span ref="toSingleMinute"></span> och
+      {{ secondsText[seconds] }} <span ref="toSingleSecond"></span> kvar
+    </h1>
+  </section>
 </template>
 
 <script>
 export default {
-name: 'TextTimer',
- props: {
+  name: 'TextTimer',
+  props: {
     minutes: Number,
-    seconds: Number
+    seconds: Number,
   },
-data() {
-    
+  data() {
     return {
-        secondsText :["noll", "en", "två","tre","fyra", "fem","sex", "sju", "åtta", "nio","tio", "elva", "tolv", "tretton", "fjorton", "femton", "sexton", "sjutton", "arton", "nitton",  
-        "tjugo", "tjugoen", "tjugotvå", "tjugotre", "tjugofyra", "tjugofem", "tjugosex", "tjugosju", "tjugoåtta", "tjugonio",
-        "trettio","trettioen", "trettiotvå", "trettiotre", "trettiofyra", "trettiofem", "trettiosex", "trettiosju", "trettioåtta", "trettionio", 
-        "fyrtio", "fyrtioen","fyrtiotvå","fyrtiotre","fyrtiofyra","fyrtiofem","fyrtiosex","fyrtiosju","fyrtioåtta","fyrtionio",
-        "femtio", "femtioen", "femtiotvå", "femtiotre", "femtiofyra", "femtiofem", "femtiosex", "femtiosju", "femtioåtta", "femtionio",],
+      secondsText: [
+        'noll',
+        'en',
+        'två',
+        'tre',
+        'fyra',
+        'fem',
+        'sex',
+        'sju',
+        'åtta',
+        'nio',
+        'tio',
+        'elva',
+        'tolv',
+        'tretton',
+        'fjorton',
+        'femton',
+        'sexton',
+        'sjutton',
+        'arton',
+        'nitton',
+        'tjugo',
+        'tjugoen',
+        'tjugotvå',
+        'tjugotre',
+        'tjugofyra',
+        'tjugofem',
+        'tjugosex',
+        'tjugosju',
+        'tjugoåtta',
+        'tjugonio',
+        'trettio',
+        'trettioen',
+        'trettiotvå',
+        'trettiotre',
+        'trettiofyra',
+        'trettiofem',
+        'trettiosex',
+        'trettiosju',
+        'trettioåtta',
+        'trettionio',
+        'fyrtio',
+        'fyrtioen',
+        'fyrtiotvå',
+        'fyrtiotre',
+        'fyrtiofyra',
+        'fyrtiofem',
+        'fyrtiosex',
+        'fyrtiosju',
+        'fyrtioåtta',
+        'fyrtionio',
+        'femtio',
+        'femtioen',
+        'femtiotvå',
+        'femtiotre',
+        'femtiofyra',
+        'femtiofem',
+        'femtiosex',
+        'femtiosju',
+        'femtioåtta',
+        'femtionio',
+      ],
 
-        minutesText :["noll", "en", "två","tre","fyra", "fem","sex", "sju", "åtta", "nio","tio", "elva", "tolv", "tretton", "fjorton", "femton", "sexton", "sjutton", "arton", "nitton",  
-        "tjugo", "tjugoen", "tjugotvå", "tjugotre", "tjugofyra", "tjugofem", "tjugosex", "tjugosju", "tjugoåtta", "tjugonio",
-        "trettio","trettioen", "trettiotvå", "trettiotre", "trettiofyra", "trettiofem", "trettiosex", "trettiosju", "trettioåtta", "trettionio", 
-        "fyrtio", "fyrtioen","fyrtiotvå","fyrtiotre","fyrtiofyra","fyrtiofem","fyrtiosex","fyrtiosju","fyrtioåtta","fyrtionio",
-        "femtio", "femtioen", "femtiotvå", "femtiotre", "femtiofyra", "femtiofem", "femtiosex", "femtiosju", "femtioåtta", "femtionio",],
-
-      
+      minutesText: [
+        'noll',
+        'en',
+        'två',
+        'tre',
+        'fyra',
+        'fem',
+        'sex',
+        'sju',
+        'åtta',
+        'nio',
+        'tio',
+        'elva',
+        'tolv',
+        'tretton',
+        'fjorton',
+        'femton',
+        'sexton',
+        'sjutton',
+        'arton',
+        'nitton',
+        'tjugo',
+        'tjugoen',
+        'tjugotvå',
+        'tjugotre',
+        'tjugofyra',
+        'tjugofem',
+        'tjugosex',
+        'tjugosju',
+        'tjugoåtta',
+        'tjugonio',
+        'trettio',
+        'trettioen',
+        'trettiotvå',
+        'trettiotre',
+        'trettiofyra',
+        'trettiofem',
+        'trettiosex',
+        'trettiosju',
+        'trettioåtta',
+        'trettionio',
+        'fyrtio',
+        'fyrtioen',
+        'fyrtiotvå',
+        'fyrtiotre',
+        'fyrtiofyra',
+        'fyrtiofem',
+        'fyrtiosex',
+        'fyrtiosju',
+        'fyrtioåtta',
+        'fyrtionio',
+        'femtio',
+        'femtioen',
+        'femtiotvå',
+        'femtiotre',
+        'femtiofyra',
+        'femtiofem',
+        'femtiosex',
+        'femtiosju',
+        'femtioåtta',
+        'femtionio',
+      ],
     }
-},
-watch: {
-    minutes(){
-         this.handleSingleValue()
+  },
+  watch: {
+    minutes() {
+      this.handleSingleMinute()
     },
     seconds() {
-        this.handleSingleValue()
-    }
-},
-methods: {
-    handleSingleValue() {
-      
-      if (this.minutes  === 1 ) {
-        this.$refs.toSingleMinute.innerText = 'minut';
-      } else if (this.seconds  === 1 ) {
-        this.$refs.toSingleSecond.innerText = 'sekund';
-      }
-       else  {
-        this.$refs.toSingleMinute.innerText = 'minuter';
-          this.$refs.toSingleSecond.innerText = 'sekunder';
+      this.handleSingleSecond()
+    },
+  },
+  methods: {
+    handleSingleMinute() {
+      if (this.minutes == 1) {
+        this.$refs.toSingleMinute.innerText = 'minut'
+      } else {
+        this.$refs.toSingleMinute.innerText = 'minuter'
       }
     },
-},
 
+    handleSingleSecond() {
+      if (this.seconds == 1) {
+        this.$refs.toSingleSecond.innerText = 'sekund'
+      } else {
+        this.$refs.toSingleSecond.innerText = 'sekunder'
+      }
+    },
+  },
 }
 </script>
 
 <style scoped>
-
 .digital {
   width: 100vw;
   height: 100vh;
@@ -70,6 +184,4 @@ methods: {
   font-size: 4em;
   text-transform: uppercase;
 }
-
-
 </style>
